ea73b84b0c040c243947194e8ab3d6d9876501ee
eire :: eireball <eireball@proton.me>
2026-08-21 18:43:32 +0200

    removing spa belgian bath

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import spaFallback from 'vite-plugin-spa-fallback'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [
     vue(),
-    spaFallback()
   ],
 
   base: '/',
