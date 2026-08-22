43ff60d4f3939f23cb8c845c63ff0bcefd856566
eire :: eireball <eireball@proton.me>
2026-08-22 09:37:13 +0200

    racing wont take you to raing, now it will

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import GamesView from '../views/GamesView.vue'
-import AboutMeView from '../views/AboutMeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import GamesView from "../views/GamesView.vue";
+import AboutMeView from "../views/AboutMeView.vue";
+import GameRacingView from "../views/GameRacingView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
-      meta: { title: 'eire' }
+      meta: { title: "eire" },
     },
     {
-      path: '/me',
-      name: 'about-me',
+      path: "/me",
+      name: "about-me",
       component: AboutMeView,
-      meta: { title: 'about me - eire' }
+      meta: { title: "about me - eire" },
     },
     {
-      path: '/games',
-      name: 'games',
+      path: "/games",
+      name: "games",
       component: GamesView,
-      meta: { title: 'games - eire' }
+      meta: { title: "games - eire" },
     },
     {
-      path: '/games/racing',
-      name: 'game-racing',
-      component: () => import('../views/GameRacingView.vue'),
+      path: "/games/racing",
+      name: "game-racing",
+      component: GameRacingView,
       meta: {
-        title: 'eiracing :: lane runner',
-        hideChrome: true
-      }
+        title: "eiracing :: lane runner",
+        hideChrome: true,
+      },
     },
     {
-      path: '/games/flagquiz',
-      name: 'game-flagquiz',
-      component: () => import('../views/FlagQuizView.vue'),
+      path: "/games/flagquiz",
+      name: "game-flagquiz",
+      component: () => import("../views/FlagQuizView.vue"),
       meta: {
         title: "eire's flag quiz",
-        hideChrome: true
-      }
-    }
+        hideChrome: true,
+      },
+    },
   ],
   scrollBehavior() {
-    return { top: 0 }
-  }
-})
+    return { top: 0 };
+  },
+});
 
 router.afterEach((to) => {
-  document.title = to.meta.title || 'eire'
-})
+  document.title = to.meta.title || "eire";
+});
 
-export default router
+export default router;
